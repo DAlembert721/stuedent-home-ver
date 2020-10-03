44b61e6b1dff583882fd39ba6089a2b54c0a38e9
Dalembert Monzon <DDMU@users.noreply.github.com>
2020-10-03 01:30:37 -0500

    dasdas

--- src/test/java/com/acme/cucumber/CreatePropertySteps.java
+++ src/test/java/com/acme/cucumber/CreatePropertySteps.java
@@ -5,7 +5,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
 public class CreatePropertySteps {
+
+    final static String ROOT_URI = "http://localhost:8080/api";
+    Response response;
+    RequestSpecification httpRequest = RestAssured.given();
+
     @Given("a landlord in the register property's view")
     public void aLandlordInTheRegisterPropertySView() {
     }
@@ -14,10 +25,13 @@ public class CreatePropertySteps {
 
     @When("the landlord clicks the register button")
     public void theLandlordClicksTheRegisterButton() {
+
+
     }
 
     @And("make a post request to {string}")
-    public void makeAPostRequestTo(String arg0) {
+    public void makeAPostRequestTo(String url) {
+        response = httpRequest.post(ROOT_URI + url);
     }
 
     @Then("the system promotes the ad of the property")
@@ -25,7 +39,9 @@ public class CreatePropertySteps {
     }
 
     @And("the result received has a status code of {int}")
-    public void theResultReceivedHasAStatusCodeOf(int arg0) {
+    public void theResultReceivedHasAStatusCodeOf(int code) {
+        Assert.assertEquals(code,response.getStatusCode());
+        System.out.println("The body of post: ->  " + response.getBody().asString());
     }
 
 
@@ -36,9 +52,12 @@ public class CreatePropertySteps {
 
     @And("the information entered is correct")
     public void theInformationEnteredIsCorrect() {
+
+        httpRequest.contentType(ContentType.JSON).body();
     }
 
     @And("the information entered is incorrect")
     public void theInformationEnteredIsIncorrect() {
+        httpRequest.contentType(ContentType.JSON).body();
     }
 }
